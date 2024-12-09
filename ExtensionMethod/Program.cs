8093132f3047477ca65a7e4c3bc66ac64dfe5cfb
Trần Quốc Cường <quoccuong0330@gmail.com>
2024-12-09 15:12:29 +0700

    ExtensionMethod

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿    
+static class ArrayExtension {
+    public static int[] FindOldInArray(this int[] array) {
+        int[] temp = [];
+        foreach (var i in array) {
+            if (i % 2 != 0) continue;
+            Array.Resize(ref temp, temp.Length + 1);
+            temp[^1] = i;
+        }
+        return temp;
+    }
+
+    public static void ListInArray(this int[] array) {
+        foreach (var i in array) {
+            Console.Write(i + "  ");
+        }
+    }
+}
+
+class Program {
+    //Extension su dung static va tu khoa this de mo rong cac phuong thuc cho doi tuong
+    static void Main() {
+        int[] array = { 1, 2, 3, 4, 5 };
+        int[] oldArray = array.FindOldInArray();
+        oldArray.ListInArray();
+    }
+}
